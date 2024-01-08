f186e2a7ef09601d7d243dfd069a1579b11294a0
Ganti Sai Sagar <sagarkr1109@gmail.com>
2024-01-09 01:47:42 +0530

    branch update

--- src/MajorityElement.java
+++ src/MajorityElement.java
@@ -1,2 +1,17 @@
 public class MajorityElement {
+    public static void main(String[] args) {
+        int[] nums = {};
+        System.out.println(logic(nums));
+    }
+    static int logic(int[] nums){
+        int count = 0;
+        int element = 0;
+        for (int i : nums) {
+            if(count==0) element = nums[i];
+            else if (element == nums[i]) {
+                count++;
+            } else count--;
+        }
+        return element;
+    }
 }
