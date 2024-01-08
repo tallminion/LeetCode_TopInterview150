b3f9f3c2fdf40c7cc31bd61514c9663f897b5e90
Ganti Sai Sagar <sagarkr1109@gmail.com>
2024-01-09 03:32:43 +0530

    #169

--- src/MajorityElement.java
+++ src/MajorityElement.java
@@ -1,12 +1,12 @@
 public class MajorityElement {
     public static void main(String[] args) {
-        int[] nums = {};
+        int[] nums = {3, 2, 3};
         System.out.println(logic(nums));
     }
     static int logic(int[] nums){
         int count = 0;
         int element = 0;
-        for (int i : nums) {
+        for (int i = 0; i < nums.length; i++) {
             if(count==0) element = nums[i];
             else if (element == nums[i]) {
                 count++;
